f71480c27656d5cd95eff4f23a0b7db0a9baa42c
joygram <joygram@gmail.com>
2023-04-03 19:58:41 +0900

    add handson and description

---
+++ handson.py
@@ -0,0 +1,40 @@
+import os 
+import pandas as pd 
+
+print(">> set api key")
+# 여기에 openai api key를 넣습니다. 
+os.environ["OPENAI_API_KEY"] = "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"
+
+print (">> read tain data 'train.csv'")
+df = pd.read_csv("./data/train.csv")
+
+print (">> head of data frame")
+print(df.head())
+
+
+
+print (">> run langchain agent ")
+from langchain.agents import create_csv_agent
+from langchain.llms import OpenAI
+
+agent = create_csv_agent(OpenAI(temperature=0), './data/train.csv', verbose=True)
+
+print(agent) 
+
+print (">> 프롬프트 템플릿(prompt template)")
+print(agent.agent.llm_chain.prompt.template)
+
+
+agent.run("데이터에 몇 열이 있어?")
+
+
+agent.run("여자는 몇명이나 있지?")
+
+agent.run("현재 도시에서 3년 넘게 지내고 있는  사람은 몇명?")
+
+agent.run("현재 도시에서 3년 넘게 지내고 있는  사람 중 여자는 몇명?")
+
+agent.run("구매횟수의 분포가 가장 높은 여자들의 분포는?")
+
+agent.run("구매횟수의 분포가 가장 높은 여자들의 분포를 그래프로 그리면?")
+
